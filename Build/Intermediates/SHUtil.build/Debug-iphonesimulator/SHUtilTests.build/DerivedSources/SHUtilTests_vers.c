0e181a8e34da365358300627d2682ae3629cc9fe
okasho <okasho1010@gmail.com>
2016-01-31 03:15:28 +0900

    NSURLの拡張を反映させるよ

--- Build/Intermediates/SHUtil.build/Debug-iphonesimulator/SHUtilTests.build/DerivedSources/SHUtilTests_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char SHUtilTestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:SHUtilTests  PROJECT:SHUtil-1" "\n"; const double SHUtilTestsVersionNumber __attribute__ ((used)) = (double)1.;
--- Build/Products/Debug-iphonesimulator/SHUtil.framework/Headers/SHUtil.h
+++ Build/Products/Debug-iphonesimulator/SHUtil.framework/Headers/SHUtil.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char SHUtilVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SHUtil/PublicHeader.h>
 
-
+#import "SHUtil-Swift.h"
+#import "SHURL-Swift.h"
